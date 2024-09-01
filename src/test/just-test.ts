bf9209c5c9ea0c6ea5c9c1cd7901400ce401e2be
julien <zhuxiang3180@gmail.com>
2024-09-01 23:15:51 +0800

    chore: typo corrected

---
+++ src/test/just-test.ts
@@ -0,0 +1,10 @@
+// ? 1. loose equals
+// const a = new Number(2)
+// let b = 2
+// Number.prototype.valueOf = function () {
+//   return b++
+// }
+// console.log('a:', a)
+// if (a == 2 && a == 3) {
+//   console.log('yeah')
+// }
